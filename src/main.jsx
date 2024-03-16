724c35453f854c361e505784b01c951c642a5041
VitaliiShc <vitaliy.shchukin@gmail.com>
2024-03-16 10:53:57 +0200

    edited confogs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
+import './index.css';
+import 'modern-normalize';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'modern-normalize';
-import App from './components/App/App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
