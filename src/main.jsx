c4339c1a6939923413d8d064eaf9e0740233fce0
VitaliiShc <vitaliy.shchukin@gmail.com>
2024-03-28 20:56:29 +0200

    add libs

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import './index.css';
-import 'modern-normalize';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
